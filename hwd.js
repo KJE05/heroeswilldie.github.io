6fbf2d3093c38b8c5aaa76b2764f57715912c7c7
in-universe-create <101760141+in-universe-create@users.noreply.github.com>
2023-05-08 14:56:31 +0900

    Update hwd.js

--- hwd.js
+++ hwd.js
@@ -6,7 +6,7 @@ window.onload = function () {
     for (let i = 0; i < rowCnt; i++) {
         document.write('<tr>');
             for (let j = 0; j < columnCnt; j++)  {
-                var tdId = "hwdLight"+j;
+                var tdId = j;
                 document.write('<td class="hwd" id='+tdId+'>');
                 document.write("");
                 document.write('</td>');
@@ -16,12 +16,16 @@ window.onload = function () {
     document.write('</table>');
     
     var body = document.querySelector("body");
-    var hwd = document.querySelector("td");
+    var hwd = document.getElementsByClassName("hwd");
     hwd.style.border = "1px solid #ffffff"; 
     body.style.backgroundColor = "#000000";
     
     var clicktest = document.querySelector("td");
     clicktest.onclick = function (event){
-        console.log(this.id);
+        if(event.target.value){
+            event.target.style.backgroundColor ="#fff671";
+        }else{
+            event.target.style.backgroundColor ="#000000";
+        }
      }
   }
